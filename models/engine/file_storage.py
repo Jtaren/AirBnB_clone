f5ff543e6466ed0cc02cf088c18f56b17031744a
james-eo <jameseokonkwo@gmail.com>
2024-02-11 19:51:17 +0100

    Deserialisation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""
-Module containing the FileStorage class
+"""Module containing the FileStorage class.
+
+This module contains the FileStorage class which
+serializes instances to a JSON file
+and deserializes JSON files to instances.
 """
 
 import json
@@ -8,8 +11,12 @@ from models import base_model
 
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances
+    """Serializes instances to a JSON file and
+    deserializes JSON file to instances.
+
+    Attributes:
+        __file_path (str): The file path to the JSON file.
+        __objects (dict): A dictionary to store serialized objects.
     """
 
     __file_path = "file.json"
@@ -20,24 +27,28 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        """Sets in __objects the obj with key <obj class name>.id.
+
+        Args:
+            obj: The object to be added to __objects
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
         with open(self.__file_path, "w", encoding="utf-8") as f:
-            data = {k: v.to_dict() for k, v in self.__objects.items()}
-            json.dump(data, f)
+            d = {k: v.to_dict() for k, v in self.__objects.items()}
+            json.dump(d, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
-                dict_obj = json.load(f)
-                for i in dict_obj.values():
-                    cls_name = i["__class__"]
-                    del i["__class__"]
-                    self.new(eval(f"base_model.{cls_name}")(**i))
+                obj_dict = json.load(f)
+                for o in obj_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(f"base_model.{cls_name}")(**o))
         except FileNotFoundError:
             pass
