077557b068e09c66c4e63b6799bf123462e9b525
james-eo <jameseokonkwo@gmail.com>
2024-02-11 10:56:01 +0100

    Documented BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
+"""Defines a parent class Basemodel"""
 
 import uuid
 from datetime import datetime
+import models
 
 
-class BaseModel():
+class BaseModel:
+    """Class Basemodel is a parent class that defines all common
+    attributes and methods for other classes
+
+    Methods:
+        -__init__(): initialises the self when called
+        -__str__(): prints the class name
+        -save(): updates and saves the instance
+        -to_dict(): returns a dictionary representation of the instance
+    """
 
     def __init__(self, *args, **kwargs):
+        """Initialises the instance attributes"""
 
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -20,14 +32,24 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns a tring representaion of the instance"""
+        return f"{[self.__class__.__name__]}, {self.id}, {self.__dict__}"
 
     def save(self):
+        """Updates and saves the instance when updated"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+        """Generates a dictionary representation of an instance.
+
+        Returns:
+            A dictionary containing key-value pairs
+            of the instance attributes
+        """
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
